213cd6bbe9ab3c13680ad80b6019b51bca7ca50f
Anshu <anshuraikwar99@gmail.com>
2024-04-28 17:04:50 +0530

    setup for deployement with github-pages

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,14 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslint(),
-  react(),
-  svgr({
-    svgrOptions: {
-      // svgr options
-    },
-  }),],
+  base: "/Rollout",
+  plugins: [
+    eslint(),
+    react(),
+    svgr({
+      svgrOptions: {
+        // svgr options
+      },
+    }),
+  ],
 })
